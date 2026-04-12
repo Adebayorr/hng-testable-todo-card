ca7874f7f85bf9e889120c6bcc8b61618b7df991
Adebayorr <raheemadebayorr@gmail.com>
2026-04-12 23:01:19 +0100

    fixed countdown elements and other fixes

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const completionCheck = document.body.querySelector('.completion[data-testid="test-todo-complete-toggle"]')
 const statusElement = document.body.querySelector('.status')
 const titleElement = document.body.querySelector('.title')
+const countdown = document.querySelector('.countdown')
 const timeLeftElement = document.body.querySelector('[data-testid="test-todo-time-remaining"]')
 const buttons = document.querySelectorAll('button')
 
+
 buttons.forEach(button => button.addEventListener('click', (e) => {
-    console.log('Action Successful')
+    console.log(`${button.textContent} clicked`)
 }))
 
 console.log(buttons)
@@ -14,14 +16,17 @@ console.log(timeLeftElement)
 
 completionCheck.addEventListener('change', (e) => {
     if (e.target.checked) {
-        statusElement.innerHTML = 'Completed'
+        statusElement.innerHTML = 'Done'
         statusElement.classList.remove('status-red')
         statusElement.classList.add('status-green')
         titleElement.classList.add('completed')
+        countdown.classList.add('rm-countdown')
+
     } else {
-        statusElement.innerHTML = 'pending'
+        statusElement.innerHTML = 'Pending'
         titleElement.classList.remove('completed')
         statusElement.classList.remove('status-green')
+        console.log(countdown.classList.remove('rm-countdown'))
 
     }
 })
@@ -38,22 +43,20 @@ function updateCountDown () {
         let hours = timeLeft % (1000 * 24 * 60 *  60 ) / (1000 * 60 * 60)
         let minutes = timeLeft %  (1000 * 24 * 60 *  60 ) / (1000 * 60 * 60)
         console.log(days, hours)
-        timeLeftElement.innerHTML = `Due in ${Math.floor(days)} days ${Math.floor(hours)} hours`
+        timeLeftElement.innerHTML = `Due in ${Math.floor(Math.abs(days))} days
+         ${Math.floor(Math.abs(hours))} hours and ${Math.floor(Math.abs(minutes))} minutes`
         return `${Math.floor(days)} days ${Math.floor(hours)} hours`
     } else {
         let days = timeLeft / (1000 * 24 * 60 * 60)
         let hours = timeLeft % (1000 * 24 * 60 *  60 ) / (1000 * 60 * 60)
-        let minutes = timeLeft %  (1000 * 24 * 60 *  60 ) / (1000 * 60 * 60)
+        let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
         console.log(days, hours)
-        timeLeftElement.innerHTML = `Overdue by ${Math.abs(Math.floor(days))} days ${Math.abs(Math.floor(hours))} hours`
+        timeLeftElement.innerHTML = `Overdue by ${Math.floor(Math.abs(days))} days ${Math.floor(Math.abs(hours))} hours and ${Math.floor(Math.abs(minutes))} minutes`
         statusElement.innerHTML = 'Expired'
         statusElement.classList.add('status-red')
-        return `${Math.abs(Math.floor(days))} days ${Math.abs(Math.floor(hours))} hours`
+        return `${Math.floor(Math.abs(days))} days ${Math.floor(Math.abs(hours))} hours and ${Math.floor(Math.abs(minutes))} minutes`
     }
     
 }
-
+updateCountDown()
 setInterval(updateCountDown, 60000) //setInterval(updateCountDown, 60000)
-
-
-console.log(updateCountDown())
